4245d5ea6139bdb903c0d7168b8923427b174b04
Ramanan <ratram2010@gmail.com>
2023-09-06 20:44:15 +0100

    roboshop terraform vpc

---
+++ subnets/main.tf
@@ -0,0 +1,20 @@
+resource "aws_subnet" "main" {
+  for_each          = var.subnets
+  vpc_id            = var.vpc_id
+  cidr_block        = each.value["cidr"]
+  availability_zone = each.value["az"]
+  tags = merge(locals.tags, { Name = "${var.env}-${each.key}-subnet" })
+
+}
+
+resource "aws_route_table" "main" {
+  for_each = var.subnets
+  vpc_id   = var.vpc_id
+  tags = merge(locals.tags, { Name = "${var.env}-${each.key}-rt" })
+}
+
+resource "aws_route_table_association" "a" {
+  for_each       = var.subnets
+  subnet_id      = lookup(lookup(aws_subnet.main, each.key, null), "id", null)
+  route_table_id = lookup(lookup(aws_route_table.main, each.key, null), "id", null)
+}
